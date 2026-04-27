65910b5c245fe90af1510e5b79ae77a72015dbdd
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-04-27 23:36:02 +0100

    inheritance and db

--- Backend/src/main/java/net/omaima/backend/entities/BankAccount.java
+++ Backend/src/main/java/net/omaima/backend/entities/BankAccount.java
@@ -5,15 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.omaima.backend.enums.AccountStatus;
-import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 import java.util.List;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="TYPE",length = 4)
 @Data @NoArgsConstructor
 @AllArgsConstructor
-public abstract class BankAccount {
+public class BankAccount {
     @Id
     private String id;
     private double balance;
--- Backend/src/main/java/net/omaima/backend/entities/CurrentAccount.java
+++ Backend/src/main/java/net/omaima/backend/entities/CurrentAccount.java
@@ -1,10 +1,15 @@
 package net.omaima.backend.entities;
 
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@DiscriminatorValue("CA")
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class CurrentAccount extends BankAccount {
