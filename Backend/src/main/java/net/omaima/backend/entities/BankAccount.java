92623981ca1c95b61451754545c4b859116bce29
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-05-01 19:27:45 +0100

    integrating mysql

--- Backend/src/main/java/net/omaima/backend/entities/BankAccount.java
+++ Backend/src/main/java/net/omaima/backend/entities/BankAccount.java
@@ -10,10 +10,10 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-//@DiscriminatorColumn(name="TYPE",length = 4)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="TYPE",length = 4)
 @Data @NoArgsConstructor
-@AllArgsConstructor  //pour ne creer que les classes derivees .
+@AllArgsConstructor
 public abstract class BankAccount {
     @Id
     private String id;
