87dd562f09588a446095a64bb3d609ba517d586b
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-07-01 16:01:59 +0100

    using the mcp tools

--- Chatbot-telegram/src/main/java/net/omaima/chatbottelegram/agents/AIAgent.java
+++ Chatbot-telegram/src/main/java/net/omaima/chatbottelegram/agents/AIAgent.java
@@ -1,17 +1,24 @@
 package net.omaima.chatbottelegram.agents;
 
-import net.omaima.chatbottelegram.tools.AITools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class AIAgent {
     private final ChatClient chatClient;
 
     // ✅ Constructeur avec 2 paramètres uniquement
-    public AIAgent(ChatClient.Builder builder, ChatMemory memory, AITools tools) {
+    public AIAgent(ChatClient.Builder builder, ChatMemory memory, ToolCallbackProvider tools) {
+        Arrays.stream(tools.getToolCallbacks()).forEach(toolCallback -> {
+            System.out.println("------------------------------");
+            System.out.println(toolCallback.getToolDefinition());
+            System.out.println("------------------------------");
+        });
         this.chatClient = builder
                 .defaultSystem("""
                         Vous êtes un assistant qui répond aux questions
@@ -19,7 +26,7 @@ public class AIAgent {
                         Si aucun contexte n'est fourni, répondez "JE NE SAIS PAS".
                         """)
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
-                .defaultTools(tools)
+                .defaultToolCallbacks(tools)
                 .build();
     }
 
